0057bcf6e9c7f40eebee481ba0fc266a62454d5d
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2023-02-14 17:16:17 +0500

    Layout complete

---
+++ src/components/Game.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import "../Game.css"
+
+function TicTacToeGame() {
+
+    return (
+        <div className='Board'>
+            <div className='Row'>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+            </div>
+            <div className='Row'>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+            </div>
+            <div className='Row'>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+                <div className='Column'>
+                    ffff
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default TicTacToeGame;
